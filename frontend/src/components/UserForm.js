da2b7bd04061e38661ebab8d9f96ece32147710e
PatKuz <pkuzdzal@bu.edu>
2022-04-08 12:35:05 -0400

    fixed linking issue between backend and frontend

--- frontend/src/components/UserForm.js
+++ frontend/src/components/UserForm.js
@@ -3,26 +3,23 @@ import React, {useState} from 'react';
 export default function UserForm() {
     const [location, setLocation] = useState('');
     const [data, setData] = useState({});
+    const axios = require('axios');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         // This API call is currently failing due to CORS cross-origin issue
         // add response headers to fix this?
-        fetch('http://localhost:5000/api/search/' + location,
+        axios.get('http://127.0.0.1:5000/api/search/' + location,
             {
             mode: 'no-cors',
             method: 'GET',
-            origin: 'http://localhost:3000',
+            origin: 'http://127.0.0.1:3000',
             headers : {
                 'Content-Type':'application/json'
             },
-        })
-        .then(res => {
-            console.log(res.json);
-            res.json()})
-        .then(data => {
-            setData(data);
-            console.log(data);
+        }).then(resp => {
+
+            console.log(resp.data);
         })
         .catch(err => console.log(err));
     }
@@ -36,12 +33,14 @@ export default function UserForm() {
                 <input type="submit" />
             </form>
             <br></br>
+            {data ? 
             <div>
                 <p>Current location: {data.location}</p>
                 <p>Weather: {data.weather}</p>
                 <p>Sentiment Val: {data.sentiment}</p>
                 <p>Playlists: {data.playlists}</p>
             </div>
+            : <></>}
         </div>
     )
 }
