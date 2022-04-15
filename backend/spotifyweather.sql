3bf29b231ec111a0b0f08862e6a40ee4b3a48a95
Andrew Tuckman <tuckman@bu.edu>
2022-04-15 11:52:45 -0400

    Update spotifyweather.sql

--- backend/spotifyweather.sql
+++ backend/spotifyweather.sql
@@ -5,18 +5,18 @@ DROP TABLE IF EXISTS Playlists CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE Users (
-	user_id INT NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL PRIMARY KEY,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL
     );
     
 CREATE TABLE Playlists (
-	playlist_id INT NOT NULL PRIMARY KEY,
+    playlist_id INT NOT NULL PRIMARY KEY,
     user_id INT NOT NULL,
     FOREIGN KEY (user_id)
-		REFERENCES Users (user_id)
+	REFERENCES Users (user_id)
         ON DELETE CASCADE,
-	link VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL,
     location VARCHAR(255),
     temp FLOAT,
     weather_condition VARCHAR(255),
