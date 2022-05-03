f7077051f55bd65093b11b7ff8061d191a6cae0d
Andrew Tuckman <tuckman@bu.edu>
2022-05-03 03:13:48 -0400

    Update sql to use email as PK for users

--- backend/spotifyweather.sql
+++ backend/spotifyweather.sql
@@ -5,19 +5,19 @@ DROP TABLE IF EXISTS Playlists CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE Users (
-    user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(50) NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
     password VARCHAR(50)
     );
     
 CREATE TABLE Playlists (
     playlist_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id)
-		REFERENCES Users (user_id)
+    email VARCHAR(50) NOT NULL,
+    FOREIGN KEY (email)
+	REFERENCES Users (email)
         ON DELETE CASCADE,
-	link VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL,
     location VARCHAR(255),
     temp FLOAT,
     weather_condition VARCHAR(255),
